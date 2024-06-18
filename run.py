674d69b02a57add0448fa7836e9c2d75841f601e
Alice Chan <alicebay10@yahoo.co.uk>
2024-06-18 22:53:12 +0000

    Adjust after validation

--- run.py
+++ run.py
@@ -1,13 +1,11 @@
 import gspread
 from google.oauth2.service_account import Credentials
-from pprint import pprint
 
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
     "https://www.googleapis.com/auth/drive.file",
     "https://www.googleapis.com/auth/drive"
     ]
-
 CREDS = Credentials.from_service_account_file('creds.json')
 SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
@@ -16,22 +14,22 @@ SHEET = GSPREAD_CLIENT.open('Vaccines - Africa (2020-2024)')
 
 def get_livessaved_data():
     """
-    Get lives saved figures input from the user.
-    Run a while loop to collect a valid string of data from the user
-    via the terminal, which must be a string of 8 numbers separated
-    by commas. The loop will repeated request data, until it is valid.
+    Get livessaved figures input from the user.
     """
     while True:
-        print("Please enter the numbers of lives saved in the last year by the following vaccines in this order: Diptheria, Hepatitis B, Measles, Polio, Rebella, Tentanus, Tiberculosis, and Yellow Fever.")
-        print("Data should be eight numbers, separated by commas.")
-        print("Example: 100,200,300,400,500,600,700,800\n")
+        print("Please enter the numbers of lives saved by the following \n\
+        vaccines in the last year in this order: \n\
+        Diphtheria, Hepatitis B, Measles, Polio, Rubella,Tetanus, \n\
+        Tuberculosis, Yellow fever.")
+        print("Data should be eight numbers, separated by commas")
+        print("Example: 1000,2000,3000,4000,5000,6000,7000,8000\n")
 
-        data_str = input("Enter your data here: ")
-
+        data_str = input("Enter your data here:\n")
         livessaved_data = data_str.split(",")
 
         if validate_data(livessaved_data):
             print("Data is valid!")
+            print(f"The data provided is {data_str}")
             break
 
     return livessaved_data
@@ -56,32 +54,27 @@ def validate_data(values):
     return True
 
 
-def update_livessaved_worksheet(data):
+def update_worksheet(data, worksheet):
     """
-    Update livessaved worksheet, add new row with the list data provided.
-    """
-    print("Updating livessaved worksheet...\n")
-    livessaved_worksheet = SHEET.worksheet("livessaved")
-    livessaved_worksheet.append_row(data)
-    print("livessaved worksheet updated successfully.\n")
+    - Receives a list of integers to be inserted into a worksheet
+    Update the relevant worksheet with the data provided.
 
-
-def update_surplus_worksheet(data):
+    -The data refer to the numbers of lives saved, and the
+    calculated numbers of surplus, vaccine production, and the
+    total lives saved.
     """
-    Update surplus worksheet, add new row with the list data provided.
-    """
-    print("Updating surplus worksheet...\n")
-    surplus_worksheet = SHEET.worksheet("surplus")
-    surplus_worksheet.append_row(data)
-    print("Surplus worksheet updated successfully.\n")
+    print(f"Updating {worksheet} worksheet...\n")
+    worksheet_to_update = SHEET.worksheet(worksheet)
+    worksheet_to_update.append_row(data)
+    print(f"{worksheet} worksheet updated successfully\n")
 
 
 def calculate_surplus_data(livessaved_row):
     """
-    Compare lives saved number with vaccine produce number
-    and calculate the surplus for each vaccine type.
+    - Compare lives saved number with vaccine produce number.
+    Calculate the surplus for each vaccine type.
 
-    The surplus is defined as the lives saved figure subtracted
+    - The surplus is defined as the lives saved figure subtracted
     from the vaccine produce number.
     """
     print("Calculating surplus data...\n")
@@ -92,8 +85,59 @@ def calculate_surplus_data(livessaved_row):
     for vaccineproduce, livessaved in zip(vaccineproduce_row, livessaved_row):
         surplus = int(vaccineproduce) - livessaved
         surplus_data.append(surplus)
+    print(surplus_data)
 
     return surplus_data
+
+
+def get_last_5_entries_livessaved():
+    """
+    Collects collumns of data from lives saved worksheet,
+    collecting the last 5 entries for each vaccine
+    and returns the data as a list of lists.
+    """
+    livessaved = SHEET.worksheet("livessaved")
+
+    columns = []
+    for ind in range(1, 9):
+        column = livessaved.col_values(ind)
+        columns.append(column[-5:])
+
+    return columns
+
+
+def calculate_vaccineproduce_data(data):
+    """
+    Calculate the average vaccine produce number for each type of vaccine,
+    then adding 20%.
+    """
+    print("Calculating new vaccince production data for next year...\n")
+    new_vaccineproduce_data = []
+
+    for column in data:
+        int_column = [int(num) for num in column]
+        average = sum(int_column) / len(int_column)
+        vaccineproduce_num = average * 2.2
+        new_vaccineproduce_data.append(round(vaccineproduce_num))
+    print(new_vaccineproduce_data)
+
+    return new_vaccineproduce_data
+
+
+def calculate_totallivessaved_data(data):
+    """
+    Calculate the lump sum number for each vaccine tape from the last 5 years
+    """
+    print("Calculating total lives saved data from the last 5 years...\n")
+    new_totallivessaved_data = []
+
+    for column in data:
+        int_column = [int(num) for num in column]
+        total = sum(int_column)
+        totallivessaved_num = total
+        new_totallivessaved_data.append(totallivessaved_num)
+    print(new_totallivessaved_data)
+    return new_totallivessaved_data
 
 
 def main():
@@ -102,12 +146,15 @@ def main():
     """
     data = get_livessaved_data()
     livessaved_data = [int(num) for num in data]
-    update_livessaved_worksheet(livessaved_data)
+    update_worksheet(livessaved_data, "livessaved")
     new_surplus_data = calculate_surplus_data(livessaved_data)
-    update_surplus_worksheet(new_surplus_data)
+    update_worksheet(new_surplus_data, "surplus")
+    livessaved_columns = get_last_5_entries_livessaved()
+    vaccineproduce_data = calculate_vaccineproduce_data(livessaved_columns)
+    update_worksheet(vaccineproduce_data, "vaccineproduce")
+    totallivessaved_data = calculate_totallivessaved_data(livessaved_columns)
+    update_worksheet(totallivessaved_data, "totallivessaved")
 
 
-print("Welcome to Lives Saved Data Automation")
-
-
+print("Welcome to Vaccines Africa Data Automation")
 main()
